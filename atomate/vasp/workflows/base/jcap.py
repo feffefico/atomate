46aab589283f9742316fa1c6e4ffe0342adf0b1b
montoyjh <montoyjh@gmail.com>
2017-09-29 09:55:54 -0700

    updates for 3 slabs, conventional cell

--- atomate/vasp/workflows/base/jcap.py
+++ atomate/vasp/workflows/base/jcap.py
@@ -8,6 +8,7 @@ edge finding as part of the JCAP project.
 """
 
 import numpy as np
+from collections import OrderedDict
 
 from fireworks import Workflow, Firework
 from fireworks import FiretaskBase, FWAction, explicit_serialize
@@ -35,9 +36,8 @@ def get_hse_bandedge_wf(structure, gap_only=True, vasp_cmd='vasp',
     Function to return workflow designed to return HSE band edges.
     In development.
     """
-        
-    # Second part of workflow is an HSE run on a minimal slab,
-    # generated from the bulk structure
+    # Get conventional structure
+    structure = SpacegroupAnalyzer(structure).get_conventional_standard_structure()
     sgp = {"min_slab_size": min_slab_size,
            "min_vacuum_size": min_vacuum_size}
     slabs = generate_all_slabs(structure, max_index=max_index, **sgp)
@@ -45,17 +45,25 @@ def get_hse_bandedge_wf(structure, gap_only=True, vasp_cmd='vasp',
     # Get min volume slab, try to get symmetric slab
     slabs = sorted(slabs, key=lambda x: (x.volume, not x.is_symmetric(), 
                                          len(SpacegroupAnalyzer(x).get_symmetry_operations())))
-    slab = slabs[0]
+    # Find first three entries with distinct miller indices, not the most elegant
+    slabs_to_calculate = [slabs[0]]
+    slabs_to_calculate += [[slab for slab in slabs if slab.miller_index != slabs[0].miller_index][0]]
+    slabs_to_calculate += [[slab for slab in slabs if slab.miller_index 
+                            not in [s.miller_index for s in slabs_to_calculate]][0]]
+    assert len(slabs_to_calculate) == 3, 'Length of slabs is not 3'
 
     # TODO: decide whether we want to optimize slab, slab thickness, etc.
     # Since these are semiconductors, shouldn't need as much k-point density
-    vis = MVLSlabSet(slab, k_product=30)
-    cparams = {"vasp_cmd": vasp_cmd, "db_file": db_file}
-    slab_fws = [get_slab_fw(slab, bulk_structure=structure, vasp_input_set=vis,
-                            slab_gen_params=sgp, name="slab", **cparams)]
-    slab_fws.append(NonSCFFW(slab, parents=slab_fws[-1], name="slab", **cparams))
-    slab_fws.append(HSEBSFW(slab, parents=slab_fws[-1], name="slab_hse", **cparams))
-    wf_slab = Workflow(slab_fws)
+    slab_fws = []
+    for slab in slabs_to_calculate:
+        vis = MVLSlabSet(slab, k_product=30)
+        cparams = {"vasp_cmd": vasp_cmd, "db_file": db_file}
+        slab_fws += [get_slab_fw(slab, bulk_structure=structure, vasp_input_set=vis,
+                                 slab_gen_params=sgp, name="slab", **cparams)]
+        slab_fws.append(NonSCFFW(slab, parents=slab_fws[-1], name="slab", **cparams))
+        slab_fws.append(HSEBSFW(slab, parents=slab_fws[-1], name="slab_hse", **cparams))
+
+    wf_slab = Workflow(slab_fws, name = '{} bandedge'.format(structure.formula))
 
     if bulk:
         # Create bulk workflow and append slab WF
@@ -76,7 +84,7 @@ def get_hse_bandedge_wf(structure, gap_only=True, vasp_cmd='vasp',
     #                       fw_name_constraint='hse')
 
     # Turn on LVTOT in relevant FWs
-    slab_incar_params = {"incar_update": {"LVTOT": True, "PREC": "Normal", "ENAUG": 2000}}
+    slab_incar_params = {"incar_update": {"LVTOT": True, "PREC": "High", "ENAUG": 2000}}
     wf = add_modify_incar(wf, modify_incar_params=slab_incar_params, 
                           fw_name_constraint="slab")
 
@@ -95,8 +103,10 @@ if __name__ == "__main__":
     mp_ids = ['mp-149', 'mp-2657', 'mp-24972', 'mp-19443', 'mp-19142']
     for mp_id in mp_ids:
         structure = mpr.get_structure_by_material_id(mp_id)
+        for prop in structure.site_properties:
+            structure.remove_site_property(prop)
         wf = get_hse_bandedge_wf(structure, vasp_cmd=">>vasp_cmd<<",
                                  db_file=">>db_file<<")
         wf = add_modify_incar(wf)
-        wf = add_tags(wf, ["Bandedge_benchmark_1", mp_id])
-        #lpad.add_wf(wf)
+        wf = add_tags(wf, ["Bandedge_benchmark_2", mp_id])
+    lpad.add_wf(wf)
